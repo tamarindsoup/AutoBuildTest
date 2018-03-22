3d1842c4d53496b57a9c1dbc0a45424b048ecfbe
tamarindsoup <san.jose3993@gmail.com>
2018-03-23 00:49:05 +0900

    docker-compose.yml

--- run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-docker run -ti --rm \
-       -e DISPLAY=${DISPLAY} \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -v ${PWD}/hariboteOSByRust:/home/developer/hariboteOSByRust \
-       haribote-rust
